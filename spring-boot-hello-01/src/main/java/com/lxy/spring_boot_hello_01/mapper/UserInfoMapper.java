8d7d1a92eb2b9f13d05e3f93c54cacc75348ad2c
lxy <lxy@lxy>
2020-01-14 18:40:16 +0800

    使用xml来编写sql

--- spring-boot-hello-01/src/main/java/com/lxy/spring_boot_hello_01/mapper/UserInfoMapper.java
+++ spring-boot-hello-01/src/main/java/com/lxy/spring_boot_hello_01/mapper/UserInfoMapper.java
@@ -12,21 +12,21 @@ import com.lxy.spring_boot_hello_01.entity.UserInfo;
 @Mapper
 public interface UserInfoMapper {
 	
-	@Select("select id id, user_name userName, user_age userAge, user_birthday userBirthday, user_sex userSex, create_time createTime, update_time updateTime from user_info")
+//	@Select("select id id, user_name userName, user_age userAge, user_birthday userBirthday, user_sex userSex, create_time createTime, update_time updateTime from user_info")
 	List<UserInfo> selectAll();
 	
 	
-	@Insert("insert into "
-			+ " user_info(user_name, user_age, user_birthday, user_sex, create_time, update_time) "
-			+ " values(#{userName},#{userAge}, #{userBirthday}, #{userSex}, #{createTime}, #{updateTime})")
+//	@Insert("insert into "
+//			+ " user_info(user_name, user_age, user_birthday, user_sex, create_time, update_time) "
+//			+ " values(#{userName},#{userAge}, #{userBirthday}, #{userSex}, #{createTime}, #{updateTime})")
 	void saveUserInfo(UserInfo userInfo);
 	
-	@Update("update user_info set "
-			+ " user_name = #{userName}, "
-			+ " user_age = #{userAge}, "
-			+ " user_birthday = #{userBirthday}, "
-			+ " user_sex = #{userSex}, "
-			+ " update_Time = #{updateTime} "
-			+ " where id = #{id}")
+//	@Update("update user_info set "
+//			+ " user_name = #{userName}, "
+//			+ " user_age = #{userAge}, "
+//			+ " user_birthday = #{userBirthday}, "
+//			+ " user_sex = #{userSex}, "
+//			+ " update_Time = #{updateTime} "
+//			+ " where id = #{id}")
 	void updateUserInfo(UserInfo userInfo);
 }
