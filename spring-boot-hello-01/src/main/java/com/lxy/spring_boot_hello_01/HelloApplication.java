09167a2ccd0aaeab582e96051879a02c3ea50749
lxy <lxy@lxy>
2020-01-16 15:23:29 +0800

    spring boot设置定时任务

--- spring-boot-hello-01/src/main/java/com/lxy/spring_boot_hello_01/HelloApplication.java
+++ spring-boot-hello-01/src/main/java/com/lxy/spring_boot_hello_01/HelloApplication.java
@@ -3,9 +3,12 @@ package com.lxy.spring_boot_hello_01;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.lxy.spring_boot_hello_01.mapper")
+
+@EnableScheduling
 public class HelloApplication {
 	
 	public static void main(String[] args) {
---
+++ spring-boot-hello-01/src/main/java/com/lxy/spring_boot_hello_01/schedule/ScheduledTask.java
@@ -0,0 +1,43 @@
+package com.lxy.spring_boot_hello_01.schedule;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTask {
+	
+	private Logger logger = LoggerFactory.getLogger(ScheduledTask.class);
+
+    @Scheduled(cron = "0/10 * * * * ?") //每10秒执行一次
+    public void scheduledTaskByCorn() {
+    	logger.info("定时任务开始 ByCorn：" + new Date());
+        scheduledTask();
+        logger.info("定时任务结束 ByCorn：" + new Date());
+    }
+
+    @Scheduled(fixedRate = 10000) //每10秒执行一次
+    public void scheduledTaskByFixedRate() {
+    	logger.info("定时任务开始 ByFixedRate：" + new Date());
+        scheduledTask();
+        logger.info("定时任务结束 ByFixedRate：" + new Date());
+    }
+
+    @Scheduled(fixedDelay = 10000) //每10秒执行一次
+    public void scheduledTaskByFixedDelay() {
+    	logger.info("定时任务开始 ByFixedDelay：" + new Date());
+        scheduledTask();
+        logger.info("定时任务结束 ByFixedDelay：" + new Date());
+    }
+
+    private void scheduledTask() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
